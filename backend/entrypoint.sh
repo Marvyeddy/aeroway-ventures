02850cc758c4551757727f4be49da340fa126b6e
Marvelous Anyatonwu <anyatonwumarvelous32@gmail.com>
2026-07-28 18:03:20 +0100

    fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ set -ex
 
 uv run alembic upgrade head
 
-exec uv run fastapi run main.py --port 80 --host 0.0.0.0
+exec uv run uvicorn backend.main:app --host 0.0.0.0 --port 80
--- terraform/setup.sh
+++ terraform/setup.sh
@@ -13,7 +13,7 @@ systemctl enable docker
 if [ -n "${repo_url}" ]; then
 git clone "https://${gh_pat}@github.com/${repo_url}.git"
 # heredoc
-cat <<EOF > aeroway-ventures/backend/.env
+cat << 'EOF' > aeroway-ventures/backend/.env
 MAIL_USERNAME=${mail_username}
 MAIL_PASSWORD=${mail_password}
 MAIL_PORT=${mail_port}
@@ -27,5 +27,7 @@ JWT_SECRET_KEY=${jwt_secret_key}
 JWT_ALG=${jwt_alg}
 EOF 
 cd aeroway-ventures/backend
-docker-compose up -d --build 
+docker compose -f compose.yml up -d --build
+sleep 15
+docker compose -f compose.yml ps
 fi
